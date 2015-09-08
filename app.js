13f8f82d5f661b68ff64ad1f11e928d7dd4cc46e
EricDupertuis <dupertuis.eric@gmail.com>
2015-09-08 19:09:57 +0200

    get all urls

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var http = require('http');
 var request = require('request');
 var jsdom = require("jsdom");
 var exec = require('child_process').exec;
+var jquery = require('jquery');
 var spawn = require('child_process').spawn;
 
 var thread = process.argv[2];
@@ -20,24 +21,24 @@ var child = exec(mkdir, function (err, stdout, stderr) {
     }
 });
 
-links = [];
+var download = function (url, dest) {
+    var file = fs.createWriteStream(dest + '/' + links[i]);
+};
+
+var links = [];
+
 jsdom.env(
     thread,
     ["http://code.jquery.com/jquery.js"],
     function (err, window) {
         images = window.$(".postContainer .file .fileThumb");
         for (i = 0; i < images.length; i++) {
-            links.push(window.$(elements[i]).attr('href'));
+            links.push(window.$(images[i]).attr('href'));
         }
-        webms = window.$(".postContainer .file .fileThumb");
-        for (i = 0; i < webms.length; i++) {
-            links.push(window.$(elements[i]).attr('href'))
-        }
+       for (i = 0; i < links.length;i++) {
+           url = 'http://'+links[i].replace(/\/\//, '')
+           console.log(url);
+       }
     }
 );
 
-for (i = 0; i < links.length; i++) {
-    var file = fs.createWriteStream(folderName + '/' + links[i]);
-    file.write();
-    file.end();
-}
