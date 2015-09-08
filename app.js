f4a7d322077b01672ed14e53f80e4d37ec1863f7
EricDupertuis <dupertuis.eric@gmail.com>
2015-09-08 21:34:18 +0200

    the chmod option is useless

--- app.js
+++ app.js
@@ -8,18 +8,12 @@ var thread = process.argv[2];
 var host = url.parse(thread);
 var folderName = host.pathname.split('/').pop();
 var mkdir = 'mkdir -p ' + 'downloads/' + folderName;
-var chmod = 'chmod -R 777 downloads/' + folderName;
 
 var mkdirExec = exec(mkdir, function (err, stdout, stderr) {
     if (err) {
         throw err;
     } else {
         console.log('folder created');
-    }
-});
-var chmodExec = exec(chmod, function (err, stdout, stderr) {
-    if (err) {
-        throw err;
     }
 });
 
