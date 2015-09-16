28324f5d1304e76ac5e49279db5d788e707a4d1d
EricDupertuis <dupertuis.eric@gmail.com>
2015-09-16 09:01:38 +0200

    don't do everything in the jsdom env

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ var http = require('http-request');
 var jsdom = require("jsdom");
 var exec = require('child_process').exec;
 var argv = require('minimist')(process.argv.slice(2));
-
 var app = require('./lib/main.js');
 
-var thread = process.argv[2];
+var thread = argv["_"][0];
 var host = url.parse(thread);
-var folderName = host.pathname.split('/').pop();
+var threadName = host.pathname.split('/').pop();
 
-
-var mkdirExec = exec(app.generateDownloadFolder(argv['f']), function (err, stdout, stderr) {
+exec(app.generateDownloadFolder(argv['f'], threadName), function (err, stdout, stderr) {
     if (err) {
         throw err;
     } else {
@@ -20,32 +18,36 @@ var mkdirExec = exec(app.generateDownloadFolder(argv['f']), function (err, stdou
     }
 });
 
-var links = [];
+dom = jsdom.env(
 
-jsdom.env(
+);
+
+links = jsdom.env(
     thread,
     ["http://code.jquery.com/jquery.js"],
     function (err, window) {
+        var links = [];
         images = window.$(".postContainer .file .fileThumb");
         for (i = 0; i < images.length; i++) {
             links.push(window.$(images[i]).attr('href'));
         }
-       for (i = 0; i < links.length;i++) {
-           href = 'http://'+links[i].replace(/\/\//, '');
-           splitted = href.split("/");
-           filename = splitted[splitted.length - 1];
-           fullPath = 'downloads/' + folderName + '/' + filename;
-
-           options = {url: href};
-
-           http.get(options, fullPath, function (error, result) {
-               if (error) {
-                   console.error(error);
-               } else {
-                   console.log('File downloaded at: ' + result.file);
-               }
-           });
-       }
+        return links;
     }
 );
 
+for (i = 0; i < links.length; i++) {
+    href = 'http://'+links[i].replace(/\/\//, '');
+    splitted = href.split("/");
+    filename = splitted[splitted.length - 1];
+    fullPath = argv['f'] + '/' + filename;
+
+    options = {url: href};
+
+    http.get(options, fullPath, function (error, result) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('File downloaded at: ' + result.file);
+        }
+    });
+}
